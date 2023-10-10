2cefc67c018685603109af5d593e7ad9cb390e9c
mahdee10 <diranimahdi2002@gmail.com>
2023-10-10 11:53:02 +0200

    adasd

--- src/pages/news.js
+++ src/pages/news.js
@@ -13,7 +13,7 @@ export default function News({ data }) {
                 rel="stylesheet"
             />
             <NavBar></NavBar>
-            <section className="sm:px-24 px-5 sm:pt-5  pb-2  mx-auto 2xl:container  overflow-hidden">
+            <section className="sm:px-24 px-5 sm:pt-5  pb-2  pt-10 mx-auto 2xl:container  overflow-hidden">
                 <h1 className="text-center sm:text-4xl text-xl font-black text-white">News</h1>
             </section>
             <section className="sm:px-24 px-5 sm:pt-5 sm:pb-10 pb-14  mx-auto 2xl:container  overflow-hidden">
