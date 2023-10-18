e48424e0333acc7d6d5fd1395c85431c534ac3d7
mahdee10 <diranimahdi2002@gmail.com>
2023-10-18 09:47:30 +0200

    dafds

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,34 +4,48 @@ import Image from "next/image";
 import bg from "../../public/imgs/bg.jpg"
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import { useRef } from 'react';
+import { useRef ,useEffect} from 'react';
 import { useIsomorphicLayoutEffect } from '../helpers/isomerphicEffect';
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Banner() {
 
-
-
   const main = useRef();
+  useEffect(() => {
+    const boxes = document.querySelectorAll('.box');
+    const h2 = document.querySelector('.box h2');
 
-  useIsomorphicLayoutEffect(() => {
-    const ctx = gsap.context((self) => {
-      const boxes = self.selector('.box');
-      boxes.forEach((box) => {
-        gsap.to(box, {
-          rotation: 360,
-          scrollTrigger: {
-            trigger: box,
-            duration: 1,
-            scrub: true,
-          },
-        });
-      });
-    }, main);
-    return () => ctx.revert();
+    gsap.fromTo(boxes, {
+      opacity: 0,
+      y: 100,
+    }, {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: main.current,
+        start: 'top center', // Adjust this as needed
+        end: 'bottom center', // Adjust this as needed
+        scrub: true,
+      },
+    });
+
+    gsap.fromTo(h2, {
+      fontSize: '2rem', // Start with a smaller font size
+    }, {
+      fontSize: '6rem', // End with a larger font size
+      duration: 1,
+      scrollTrigger: {
+        trigger: main.current,
+        start: 'top center', // Adjust this as needed
+        end: 'bottom center', // Adjust this as needed
+        scrub: true,
+      },
+    });
   }, []);
-    return (
+
+      return (
 
         <div className="my-1.5 mb-4 relative image-container2 sm:py-20 py-10 flex justify-center"  ref={main}>
             <Image className="my-0 mx-2.5 w-800 h-fit rounded-full box" src={bg} alt=""   />
