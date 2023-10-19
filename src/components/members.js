806c86cc17b095cd197ba75c515ecbec83efa861
mahdee10 <diranimahdi2002@gmail.com>
2023-10-19 10:54:58 +0200

    dafds

--- src/components/members.js
+++ src/components/members.js
@@ -31,7 +31,7 @@ export default function Members(props) {
     }, []);
 
     return (
-        <div className="sm:pl-9 pl-4 sm:pr-7 pr-4 flex  rounded-xl sm:w-3/12 w-full sm:mb-0 mb-3 min-h-400">
+        <div className="sm:pl-9 pl-4 sm:pr-7 pr-4 flex sm:justify-start justify-center  rounded-xl sm:w-3/12 w-full sm:mb-0 mb-3 min-h-400">
             <div className="relative text-white w-60 h-60 rounded-full border border-fe424c min">
                 <Image ref={imageRef} className=' rounded-full w-52 h-52  absolute top-2/4 left-1/2 transform -translate-y-1/2 -translate-x-1/2' src={props.path} alt="Search Icon"></Image>
                 <div ref={lineRef} className="absolute top-full left-1/2 bg-black h-10 w-1 transform  -translate-x-1/2  bg-fe424c"></div>
